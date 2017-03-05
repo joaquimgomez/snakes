8dce13a48a49a5d00adb2c8e8485032f063045ca
DaniBM21 <dballetbo@gmail.com>
2017-03-05 18:43:15 +0100

    Update Snake.cc

--- src/Snake.cc
+++ src/Snake.cc
@@ -4,18 +4,22 @@ Snake::Snake() {
 	// Pre: cert
 	// Post: el resultat es un objecte snake al punt 0, 0
 		
-	c = Cami();
 	crash = false;
 	nSnake++;
+	
+	// snakes[nSnake].punts[nSnake].px
+	// snakes[nSnake].punts[nSnake].py
 }
 
 Snake::Snake(Punt p) {
 	// Pre: cert
 	// Post: el resultat es un objecte snake al punt p
 		
-	c = Cami();
 	crash = false;
 	nSnake++;
+	
+	// snakes[nSnake].punts[nPunts].px = p.x
+	// snakes[nSnake].punts[nPunts].py = p.y
 }
 
 Snake::~Snake(){
@@ -24,7 +28,7 @@ Snake::~Snake(){
 
 void Snake::addSnake(Punt p) {
 	// Pre: cert
-	// Post: Afegim nova serp que comença en el punt p
+	// Post: Afegim nova serp que comenÃ§a en el punt p
 	
 	Snake s = Snake(p);
 }
@@ -33,22 +37,22 @@ void Snake::longSnake(Punt s) {
 	// Pre: cert
 	// Post: Allarguem una serp ja existent amb un punt
 	
-	c.punts[npunts] = s; 
-	c.npunts++:
+	snakes[nSnake].punts[npunts] = s; 
+	snakes[nSnake].npunts++;
 }
 
 float Snake::boardWidth() const {
 	// Pre: cert
 	// Post: El resultat de l'amplada del taulell
 	
-	return c.punts[0,0].x;
+	return snakes[nSnake].punts[0,0].x;
 }
 
 float Snake::boardHeight() const {
 	// Pre: cert
 	// Post: El resultat de l'altura del taulell
 	
-	return c.punts[0,0].y;
+	return snakes[nSnake].punts[0,0].y;
 }
 
 int Snake::totalSnakes() const {
@@ -60,14 +64,14 @@ int Snake::totalSnakes() const {
 
 bool Snake::searchPoint(Punt p) const {
 	// Pre: cert
-	// Post: Donarà true si el punt donat està a l'interior de la frontera del rectangle
+	// Post: DonarÃ  true si el punt donat estÃ  a l'interior de la frontera del rectangle
 	
 	// CODIFICAR!
 }
 
 bool Snake::pointBusy(Punt p) const {
 	// Pre: cert
-	// Post: Donarà true si el punt donat ja està ocupat per una serp al taulell
+	// Post: DonarÃ  true si el punt donat ja estÃ  ocupat per una serp al taulell
 	
 	// CODIFICAR!
 }
@@ -76,7 +80,7 @@ Punt Snake::lastPoint(Snake s) const {
 	// Pre: cert
 	// Post: el darrer punt d'una serp en concret
 	
-	return s.c.punts[s.c.npunts];
+	return s.snakes[nSnake].punts[s.snakes[nSnake].nPunts];
 }
 
 
