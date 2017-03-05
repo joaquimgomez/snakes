db2e150b8f51b6bb7ce9b3ebc7212a506d05dbb7
Joaquim Gómez <9joaquimgomez@gmail.com>
2017-03-05 19:24:01 +0100

    Update main
    Bug fixes and functionality extensions.

--- main.cc
+++ main.cc
@@ -13,34 +13,47 @@ int main(){
 
   char opcio;
   int serp_actual = 0;
+  Punt p_nou;
   bool stop = false;
+
   while (not stop){
     cout << s << endl;
     cout << "Opció: Allargar (a w d x), Nova (n), Canviar serp (enter): ";
     cin << opcio;
 
 
-    if (opcio == 'a' or opcio == 'w' or opcio == 'd' or opcio == 'x'){
+    if (opcio == 'a' or opcio == 'w' or opcio == 'd' or opcio == 'x' and serp_actual >= 1){
+      if (opcio == 'a')   p_nou = s.lastPoint(serp_actual) + Punt(-1,0);
+      else if (opcio == 'w')    p_nou = s.lastPoint(serp_actual) + Punt(0,1);
+      else if (opcio == 'd')    p_nou = s.lastPoint(serp_actual) + Punt(1,0);
+      else    p_nou = s.lastPoint(serp_actual) + Punt(0,-1);
+
+      if (s.searchPoint(p_nou) and not s.pointBusy(p_nou))  s.longSnake(p_nou, serp_actual);
+      else    stop = true;
+
 
     } else if (opcio == n){
-      Punt p_nou;
-
       cout << "Punt inicial: ";
       cin << p_nou;
 
-      /* ? */ if(s.searchPoint() and not s.pointBusy())   s.addSnake(p_nou);
-      /* Si se introduce en un punto donde ya hay serpiente debe parar,
-         si el punto esta fuera de rango (?). */
+      if (s.searchPoint(p_nou) and not s.pointBusy(p_nou)){
+        s.addSnake(p_nou);
+        serp_actual = s.totalSnakes();
+      } else    stop = true;
+      /* DEBE PARAR SI SALE DE RANGO EL p_nou (?) */
 
-      serp_actual = s.totalSnakes();
 
-
-    } else if (opcio >= 49 and opcio <= 57){
+    } else if (opcio >= 49 and opcio <= 57 and serp_actual >= 1){
       if(int(opcio) <= s.totalSnakes())   serp_actual = int(opcio);
+
 
     }
 
+
   }
+
+
+  if (stop)   cout << "Col·lisió al punt " << p_nou << endl;
 
 
 }
