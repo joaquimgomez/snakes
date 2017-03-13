7d5258f15cdd4fd53d7d351299b8f29ba34778c9
Joaquim Gómez <9joaquimgomez@gmail.com>
2017-03-13 22:17:24 +0100

    Update main

--- main.cc
+++ main.cc
@@ -7,6 +7,7 @@ int main(){
 
   cout << "Amplada i altura rectangle: ";
   cin >> p;
+  cout << endl;
 
   Snake s(p);
 
@@ -17,15 +18,16 @@ int main(){
   bool stop = false;
 
   while (not stop){
-    cout << s << endl;
-    cout << "Opció: Allargar (a w s d), Nova (n), Canviar serp (enter): ";
+    cout << s;
+    cout << "Opció: Allargar (a w d s), Nova (n), Canviar serp (enter): ";
     cin >> opcio;
+    cout << endl;
 
 
-    if ((opcio == 'a' or opcio == 'w' or opcio == 's' or opcio == 'd') and serp_actual >= 1){
+    if ((opcio == 'a' or opcio == 'w' or opcio == 'd' or opcio == 's') and serp_actual >= 1){
       if (opcio == 'a')   p_nou = s.lastPoint(serp_actual) + Punt(-1,0);
       else if (opcio == 'w')    p_nou = s.lastPoint(serp_actual) + Punt(0,1);
-      else if (opcio == 's')    p_nou = s.lastPoint(serp_actual) + Punt(1,0);
+      else if (opcio == 'd')    p_nou = s.lastPoint(serp_actual) + Punt(1,0);
       else    p_nou = s.lastPoint(serp_actual) + Punt(0,-1);
 
       if (s.searchPoint(p_nou) and not s.pointBusy(p_nou))  s.longSnake(p_nou, serp_actual);
@@ -35,6 +37,7 @@ int main(){
     } else if (opcio == 'n'){
       cout << "Punt inicial: ";
       cin >> p_nou;
+      cout << endl;
 
       if (s.searchPoint(p_nou) and not s.pointBusy(p_nou)){
         s.addSnake(p_nou);
@@ -43,7 +46,7 @@ int main(){
 
 
     } else if (opcio >= 49 and opcio <= 57 and serp_actual >= 1){
-      if(int(opcio) <= s.totalSnakes())   serp_actual = int(opcio);
+      if(int(opcio) <= s.totalSnakes())   serp_actual = int(opcio); // REVISAR!
 
 
     }
